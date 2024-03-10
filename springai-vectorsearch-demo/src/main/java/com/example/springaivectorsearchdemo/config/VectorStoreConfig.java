8ab4f50f9ed6b21f624ad7724e11948a1818a410
Praveen K Singh <c2praveen30jun@gmail.com>
2024-03-10 20:28:22 +0530

    Update VectorStoreConfig.java

--- springai-vectorsearch-demo/src/main/java/com/example/springaivectorsearchdemo/config/VectorStoreConfig.java
+++ springai-vectorsearch-demo/src/main/java/com/example/springaivectorsearchdemo/config/VectorStoreConfig.java
@@ -1,6 +1,7 @@
 package com.example.springaivectorsearchdemo.config;
 import org.springframework.ai.embedding.EmbeddingClient;
-import org.springframework.ai.vectorstore.SimplePersistentVectorStore;
+//import org.springframework.ai.vectorstore.SimplePersistentVectorStore;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ public class VectorStoreConfig {
 
     @Bean
     VectorStore vectorStore(EmbeddingClient embeddingClient){
-        return new SimplePersistentVectorStore(embeddingClient);
+        // try with SimpleVectorStore or SimplePersistentVectorStore whichever is available
+       // return new SimplePersistentVectorStore(embeddingClient);
+        return new SimpleVectorStore(embeddingClient);
     }
 }
